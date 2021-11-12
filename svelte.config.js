0ace9cee479a77d8cb8f47954732ec7f1584e614
kyrenkhor315 <kyrenkhor@gmail.com>
2021-11-12 10:28:56 +0000

    Vercel deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,11 @@
-import preprocess from 'svelte-preprocess';
-/** @type {import('@sveltejs/kit').Config} */
+import vercel from '@sveltejs/adapter-vercel';
+
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
-		vite: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@import "src/variables.scss";'
-					}
-				}
-			}
-		}
-	},
-
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@import "src/variables.scss";'
-			}
-		})
-	]
+    kit: {
+   	 // hydrate the <div id="svelte"> element in src/app.html
+   	 target: '#svelte',
+   	 adapter: vercel()
+    }
 };
 
 export default config;
