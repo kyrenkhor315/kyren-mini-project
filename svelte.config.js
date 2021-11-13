a3201349160075d6d448639efee4f274fc7fdd1b
kyrenkhor315 <kyrenkhor@gmail.com>
2021-11-13 02:56:26 +0000

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,31 +1,11 @@
-import preprocess from 'svelte-preprocess';
 import vercel from '@sveltejs/adapter-vercel';
 
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
-
-		adapter: vercel(),
-
-		vite: {
-			css: {
-				preprocessorOptions: {
-					scss: {
-						additionalData: '@import "src/variables.scss";'
-					}
-				}
-			}
-		}
-	},
-
-	preprocess: [
-		preprocess({
-			scss: {
-				prependData: '@import "src/variables.scss";'
-			}
-		})
-	]
+    kit: {
+   	 // hydrate the <div id="svelte"> element in src/app.html
+   	 target: '#svelte',
+   	 adapter: vercel()
+    }
 };
 
 export default config;
